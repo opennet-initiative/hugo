818b22648a0b5dbe28691f78802329f2a4d9a455
Martin Garbe <monomartin@opennet-initiative.de>
2020-12-21 23:05:16 +0100

    only change baseurl when deploying to ruri

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@ USER=root
 HOST=ruri.on             
 DIR=/var/www/downloads.opennet-initiative.de/www-testing/
 
+#change baseurl as long as we publish under downloads.on-i.de/www-testing
+perl -pi -e 's/baseurl = .*/baseurl = "\/www-testing2\/" /g' config.toml
+#generate and copy files
 hugo && rsync -avz --delete public/ ${USER}@${HOST}:${DIR}
+#revert baseurl
+perl -pi -e 's/baseurl = .*/baseurl = "\/" /g' config.toml
 
 exit 0
