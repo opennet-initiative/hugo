568a0b1db23cc354d7fe8a5b8fb0ad57a0a97b56
Martin Garbe <monomartin@opennet-initiative.de>
2021-01-05 22:21:28 +0100

    Deployment Skript
    Aktueller RSS Content wird als Hugo Syna Fragment erstellt und
    anschliessend ins apache Verzeichnis kopiert

---
+++ on-hugo-deploy-prod.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HTDOCS=/var/www/www.opennet-initiative.de/
+
+#get RSS abstract and exit this script on failure
+md_text=$(./on-rss2md.py) || exit $?
+
+echo ${md_text} > content/posts/oni-rss-post.md
+
+cd $HUGO_PATH
+hugo && rsync -avz --delete public/ ${HTDOCS}
