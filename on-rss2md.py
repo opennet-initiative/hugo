b32ce8148ed53b0c42e96969c4d6923117f817f1
Martin Garbe <monomartin@opennet-initiative.de>
2021-01-11 21:36:40 +0100

    show only 5 blog posts and description text
    * only show 5 blog posts
    * abstract is always beginning with description text.

--- on-rss2md.py
+++ on-rss2md.py
@@ -6,6 +6,7 @@ import requests
 import xml.etree.ElementTree as ET
 import sys
 from bs4 import BeautifulSoup
+import re
 
 # download RSS
 oni_rss_feed_url='https://stadtgestalten.org/stadt/groups/16/feed/'
@@ -21,25 +22,31 @@ if rss_title.find('Opennet') == -1:
     sys.exit()
 
 out_md = "" #for Markdown output
+count = 0
 for item in tree.findall('./channel/item'):
+    if count >= 5:
+        break   #only show first 5 items
+    else:
+        count += 1
+
     title = item.find('title').text
     link = item.find('link').text
     descr = item.find('description')
 
     abstract = ""
     html = BeautifulSoup(descr.text, features="lxml")
-    for h in html.findAll('h3'):
-        abstract += h.text + ", "
+    #add colon character because after html2text conversion we need to 
+    # make clear what is headline and what is normal text.
+    for h in html.findAll(re.compile("h[1-5]")): #matches h1, h2, h3, ...
+        h.string = h.string + ": "
 
-    if len(abstract) > 0:
-        abstract = "Themen: " + abstract[:-2] # cut ", " at end
-    else:
-        #no headers found. Use text in <description> instead
-        abstract = html.get_text()[0:150]
-        #make sure last word is complete and not cut
-        abstract = abstract[0:abstract.rfind(' ')]
-        abstract += " ..."
+    abstract = html.get_text()[0:300]
+    #make sure last word is complete and not cut
+    abstract = abstract[0:abstract.rfind(' ')]
+    abstract += " ..."
+    abstract = abstract.replace("\n", " ")
+    abstract = abstract.replace("\t", " ")
 
-    out_md += ' * <a href="' + link + '">' + title + '</a> ' + abstract + '\n'
+    out_md += ' * <p style="text-align: justify"><a href="' + link + '">' + title + '</a> ' + abstract + '</p>\n'
 
 print(out_md)
